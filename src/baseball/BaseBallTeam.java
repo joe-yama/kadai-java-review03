4f8c3f334cf372d5bdf66544f35426b5474cc603
joe-yama <s1r0mqme@gmail.com>
2024-10-22 23:35:26 +0900

    Implement baseball game status report

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,59 @@
+package baseball;
+
+public class BaseBallTeam {
+  private String name;
+  private int win;
+  private int lose;
+  private int draw;
+
+  public BaseBallTeam() {}
+
+  public BaseBallTeam(String name, int win, int lose, int draw) {
+    this.name = name;
+    this.win = win;
+    this.lose = lose;
+    this.draw = draw;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getWin() {
+    return win;
+  }
+
+  public void setWin(int win) {
+    this.win = win;
+  }
+
+  public int getLose() {
+    return lose;
+  }
+
+  public void setLose(int lose) {
+    this.lose = lose;
+  }
+
+  public int getDraw() {
+    return draw;
+  }
+
+  public void setDraw(int draw) {
+    this.draw = draw;
+  }
+
+  public double getRate() {
+    return (double) win / (win + lose);
+  }
+
+  public void report() {
+    System.out.println(
+        String.format("%sの2022年の成績は %d勝 %d敗 %d分、勝率は %fです。", name, win, lose, draw, getRate()));
+  }
+
+}
